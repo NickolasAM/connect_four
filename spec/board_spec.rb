5908469934874cc6214fd9849059fb65283d8e90
Nickolas Marquez <nickomarquez7@gmail.com>
2023-02-16 18:53:04 -0700

    Created board_spec test

---
+++ spec/board_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+RSpec.describe Board do
+  let(:board) {Board.new}
+
+  it 'exists' do
+    expect(board).to be_an_instance_of(Board)
+  end
+
+
+
+
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,5 @@
+require 'rspec'
+require './lib/board'
+
+
+
