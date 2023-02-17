7ed6646720af8b74a9b96c41293e3c7741aaaf0a
Nickolas Marquez <nickomarquez7@gmail.com>
2023-02-16 19:01:04 -0700

    Has attribute_cells

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,7 +7,16 @@ RSpec.describe Board do
     expect(board).to be_an_instance_of(Board)
   end
 
+  describe '#has attributes' do
+    it "has cells" do
+      expect(board.cells).to be_an_instance_of(Hash)
+    end
 
+
+
+
+
+  end
 
 
 
