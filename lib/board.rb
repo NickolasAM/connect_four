3ae16e3fe02f4e0dc595c877dfb9368b1443d284
Nickolas Marquez <nickomarquez7@gmail.com>
2023-02-16 19:04:09 -0700

    Created cell_spec file

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,53 @@
 class Board
-  
+  attr_reader :cells
   def initialize
 
+    @cells = {
+      "A1" => Cell.new("A1"),
+      "A2" => Cell.new("A2"),
+      "A3" => Cell.new("A3"),
+      "A4" => Cell.new("A4"),
+      "A5" => Cell.new("A5"),
+      "A6" => Cell.new("A6"),
+      "B1" => Cell.new("B1"),
+      "B2" => Cell.new("B2"),
+      "B3" => Cell.new("B3"),
+      "B4" => Cell.new("B4"),
+      "B5" => Cell.new("B5"),
+      "B6" => Cell.new("B6"),
+      "C1" => Cell.new("C1"),
+      "C2" => Cell.new("C2"),
+      "C3" => Cell.new("C3"),
+      "C4" => Cell.new("C4"),
+      "C5" => Cell.new("C5"),
+      "C6" => Cell.new("C6"),
+      "D1" => Cell.new("D1"),
+      "D2" => Cell.new("D2"),
+      "D3" => Cell.new("D3"),
+      "D4" => Cell.new("D4"),            
+      "D5" => Cell.new("D5"),
+      "D6" => Cell.new("D6"),
+      "E1" => Cell.new("E1"),
+      "E2" => Cell.new("E2"),
+      "E3" => Cell.new("E3"),
+      "E4" => Cell.new("E4"),            
+      "E5" => Cell.new("E5"),
+      "E6" => Cell.new("E6"),
+      "F1" => Cell.new("F1"),
+      "F2" => Cell.new("F2"),
+      "F3" => Cell.new("F3"),
+      "F4" => Cell.new("F4"),            
+      "F5" => Cell.new("F5"),
+      "F6" => Cell.new("F6"),
+      "G1" => Cell.new("G1"),
+      "G2" => Cell.new("G2"),
+      "G3" => Cell.new("G3"),
+      "G4" => Cell.new("G4"),            
+      "G5" => Cell.new("G5"),
+      "G6" => Cell.new("G6"),
+    }
   end
+
 
 
 
