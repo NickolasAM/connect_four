2f03814dca7ae8a12fc4043f8e4b5930a4cf2d1f
Nickolas Marquez <nickomarquez7@gmail.com>
2023-02-22 16:05:46 -0700

    added horizontal win

--- lib/game.rb
+++ lib/game.rb
@@ -1,40 +1,41 @@
 class Game
-
+  attr_reader :board
   def initialize
-    @boards = Board.new
+    @board = Board.new
   end
-
   
-  def vertical_win
-    v_w = @board.columns.count_left + @board.columns.count_right >= 3
-
-  end
-
-  def horizontal_win
-    require'pry';binding.pry
-    h_w = @cellsrow.count_left + row.count_right >= 3 # 4 including the current cell
-  end
-
-  def player_verticle_win
+  def verticle_win
     column_string = []
     @board.columns.values.each do |column|
       column_string << column.join
     end
     return column_string
   end
+
+  def horizontal_win
+    row_string = []
+    @board.cells.each do |row|
+      row_value_array = []
+      row.each do |cell|
+        row_value_array << cell.value
+      end
+      row_string << row_value_array.join
+    end
+    return row_string
+  end
+  
+
+
   # def tie
   #   board.map(&:last).none? { |cell| cell == '.' }
   # end
   
-  # def win
-  #   horizontal_win || vertical_win || tie_win
-  # end
   
-  def who_wins(strings) 
-    if string.include?("xxxx")
-      puts "Player wins!"
-    elseif string.include?("oooo") 
-      puts "Computer Wins"
-    end
-  end
+  # def who_wins(strings) 
+  #   if string.include?("xxxx")
+  #     puts "Player wins!"
+  #   elseif string.include?("oooo") 
+  #     puts "Computer Wins"
+  #   end
+  # end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,11 +6,10 @@ RSpec.describe Game do
   it "exists" do
       expect(game).to be_an_instance_of(Game)
       expect(game.board).to be_an_instance_of(Board)
-
   end
 
-  describe 'check for various wins' do
-    it 'check for player verticle win' do
+  describe '#check for various wins' do
+    it 'checkaa for verticle_win' do
       game.board.columns["A"] << "x"
       game.board.columns["A"] << "x"
       game.board.columns["A"] << "x"
@@ -20,9 +19,15 @@ RSpec.describe Game do
       game.board.columns["B"] << "o"
       game.board.columns["B"] << "o"
       # require 'pry'; binding.pry
-      expect(game.player_verticle_win.include?("xxxx")).to eq true
+      expect(game.verticle_win.include?("xxxx")).to eq true
+      expect(game.verticle_win.include?("oooo")).to eq true
     end
 
+    it 'checks for horizontal_win' do
+      
+
+
+    end
   end
 
 
