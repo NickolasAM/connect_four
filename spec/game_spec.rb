438c40c21a0d97645becee2008605ff054bae4d5
Logan Wilson <loganwilson777@gmail.com>
2023-02-23 10:33:57 -0700

    Created runner file, also took out skipped tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -38,18 +38,12 @@ RSpec.describe Game do
       expect(game.diagonal_win.include?("xxxx")).to eq true
     end
 
-    xit '#check win' do
+    it '#check win' do
     game.board.cells[5][0].value = "x"
     game.board.cells[5][1].value = "x"
     game.board.cells[5][2].value = "x"
     game.board.cells[5][3].value = "x"
     expect(game.check_win).to be true
     end
-  end
-
-  it 'can play game' do
-    game.start_game
-  end
-  
-  
+  end  
 end
